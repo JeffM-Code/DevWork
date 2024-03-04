7c77c36857022d96df26ad52d513e26b9773a6da
Jeff <jefferyym1234@gmail.com>
2024-03-04 03:39:21 +0000

    cleanup

--- release-builds/db-electron-win32-x64/resources/app/external/Agarpain/include/thermal_physics_calculations.h
+++
@@ -1,7 +0,0 @@
-#ifndef THERMAL_PHYSICS_H
-#define THERMAL_PHYSICS_H
-
-void calculate_latent_heat(double mass, double latentHeatCapacity);
-void calculate_sensible_heat(double mass, double specificHeatCapacity, double temperatureChange);
-
-#endif
--- release-builds/db-electron-win32-x64/resources/app/external/Pisty/src/PistyApp/PhysicsDataProcessor.cs
+++
@@ -1,6 +0,0 @@
-namespace PistyApp
-{
-    public class PhysicsDataProcessor
-    {
-    }
-}
